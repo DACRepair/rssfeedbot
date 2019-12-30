e4567db87fdfe9d6b5cbf83afb4cd4864527d837
Derek Vance <DACRepair@gmail.com>
2019-12-29 22:01:37 -0500

    Implemented FEED and POST only

--- run.py
+++ run.py
@@ -1,12 +1,15 @@
 from App.Models import Base
 from App.tasks import app, post, store
+from App.Common.config import POST_ONLY, FEED_ONLY
 
 if __name__ == "__main__":
     try:
         Base.metadata.create_all()
 
-        app.create_task(store())
-        app.create_task(post())
+        if FEED_ONLY:
+            app.create_task(store())
+        if POST_ONLY:
+            app.create_task(post())
         app.run_forever()
     except:
         exit(1)
