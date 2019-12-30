53c1336b173167999a7a5bb80b2cadea20664f74
Derek Vance <DACRepair@gmail.com>
2019-12-29 22:48:09 -0500

    lib -> libs OOPS

--- App/Models/feed_entries.py
+++ App/Models/feed_entries.py
@@ -8,16 +8,16 @@ class FeedEntry(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     post_id = Column(String(24), unique=True)
 
-    title = Column(String(255))
-    author = Column(String(255))
+    title = Column(String(2048))
+    author = Column(String(2048))
 
-    post = Column(String(255))
+    post = Column(String(2048))
     posted_on = Column(DateTime, default=datetime.datetime.utcnow())
     text = Column(String(4096))
 
     link = Column(String(2048))
-    flair = Column(String(255))
-    thumb = Column(String(255))
+    flair = Column(String(2048))
+    thumb = Column(String(2048))
     sticky = Column(Boolean, default=False)
     nsfw = Column(Boolean, default=False)
 
